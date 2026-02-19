8be92dfcdb3693bfc5ba36d832dc0883ee8c4301
mdeniz0 <mehmet@forecr.io>
2026-02-19 09:44:26 +0300

    Power LED test for JetPack-6 updated. run_gpioset_out function simplified

--- JetPack-6/test_power_led_orin.sh
+++ JetPack-6/test_power_led_orin.sh
@@ -6,27 +6,17 @@ fi
 
 sleep_time=0.3
 
-function kill_gpioset_bg {
-	# $1 -> GPIO chip
-	# $2 -> GPIO index
-	if [ $(ps -t | grep gpioset | grep "$1 $2" | wc -l) -eq 1 ]; then
-		PID_GPIOSET_BG=$(ps -t | grep gpioset | grep "$1 $2" | awk '{print $1;}')
-
-		kill $PID_GPIOSET_BG
-
-		if [ $(ps -t | grep gpioset | grep "$1 $2" | wc -l) -eq 1 ]; then
-			echo "Unable to kill process $PID_GPIOSET_BG"
-		fi
-	fi
-}
-
 function run_gpioset_out {
 	# $1 -> GPIO chip
 	# $2 -> GPIO index
 	# $3 -> output value
 
-	# If there is a gpioset command is working in background (for the selected GPIO), kill it
-	kill_gpioset_bg $1 $2
+	# If there is a gpioset command working in background (for the selected GPIO), kill it
+	for pid in `ps -ef | grep "$1 $2" | awk '{print $2}'` ; do
+		if [ $(ps $pid | grep "gpioset" | wc -l) -ne 0 ]; then
+			kill $pid
+		fi
+	done
 
 	# Run the gpioset command in background (to keep the GPIO value continuously)
 	gpioset --mode=signal $1 $2=$3 &
